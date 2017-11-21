c852df7f38843e14fb543c8f59a0abd03b55505b
Daniel Moreto <dfmoreto@gmail.com>
2017-11-21 18:33:46 -0200

    Fix CampaignMailer spec

--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe CampaignMailer, type: :mailer do
     before do
       @campaign = create(:campaign)
       @member   = create(:member, campaign: @campaign)
+      @friend   = create(:member, campaign: @campaign)
       @mail = CampaignMailer.raffle(@campaign, @member, @friend)
     end
 
