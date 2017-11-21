04db2fceefe9d700ad147d2a11ee983eab921ecc
Daniel Moreto <dfmoreto@gmail.com>
2017-11-21 18:06:40 -0200

    Update Member factory to generate token

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
  factory :member do
    name         { FFaker::Lorem.word }
    email        { FFaker::Internet.email }
+   token        { SecureRandom.urlsafe_base64(nil, false) }
    campaign
  end
 end
